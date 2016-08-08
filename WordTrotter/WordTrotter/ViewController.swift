72aa31c97dbe121971c8b5ca79689f2a5479550c
Mason Chang <changmason@gmail.com>
2016-08-09 00:14:16 +0800

    finish ch3 about view hierarchy

---
+++ WordTrotter/WordTrotter/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WordTrotter
+//
+//  Created by Chang Ming-hsuan on 8/7/16.
+//  Copyright © 2016 Chang Ming-hsuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+
+}
+
